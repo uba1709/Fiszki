db3b38c038a093ffca620ad5a6ca3047c06103fc
uba1709 <130249082+uba1709@users.noreply.github.com>
2025-12-19 18:24:37 +0100

    01

---
+++ my-app/app/headerSection/header.tsx
@@ -0,0 +1,15 @@
+"use client";
+
+import { headers } from "next/headers";
+
+function header(){
+    return(
+        <header>
+            <nav>
+                <h1>TEst</h1>
+            </nav>
+        </header>
+
+    );
+}
+export default header;
